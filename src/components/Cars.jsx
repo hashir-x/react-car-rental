6db6d5253862af5d269ddc43152353847ab83776
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-11-28 13:19:02 +0530

    on progress

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -142,6 +142,236 @@ function Cars() {
             </div>
           </div>
         </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
+        <div className='carModels'>
+          <div>
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+          </div>
+          <div style={{padding:"1rem"}}>
+            <div className='car-second'>
+              <div>
+                <h4 style={{color:"#000"}}>Audi A1</h4>
+              </div>
+              <div>
+                <h4 style={{color:"#000"}}>$89 / day</h4>
+              </div>
+            </div>
+            <div className='car-third'>
+              <span>Petrol <i class="fa-solid fa-gas-pump ms-1"></i></span>
+              <span>Automatic <i class="fa-solid fa-car ms-1"></i></span>
+            </div>
+            <hr />
+            <div className="car-button">
+              <button className='book-ride'>Book Ride</button>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
