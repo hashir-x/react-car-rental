2eec45cedb95582a3d306a3e6ceb90bf26128960
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-11-29 23:34:35 +0530

    car models

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -29,12 +29,12 @@ function Cars() {
         </div>
         <div className='carModels'>
           <div>
-            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/bmw-box.185d90ec902e13d80eea.png" alt="" />
           </div>
           <div style={{padding:"1rem"}}>
             <div className='car-second'>
               <div>
-                <h4 style={{color:"#000"}}>Audi A1</h4>
+                <h4 style={{color:"#000"}}>BMW 320</h4>
               </div>
               <div>
                 <h4 style={{color:"#000"}}>$89 / day</h4>
@@ -52,12 +52,12 @@ function Cars() {
         </div>
         <div className='carModels'>
           <div>
-            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/benz-box.a9aa68ef5eb5af1f342b.png" alt="" />
           </div>
           <div style={{padding:"1rem"}}>
             <div className='car-second'>
               <div>
-                <h4 style={{color:"#000"}}>Audi A1</h4>
+                <h4 style={{color:"#000"}}>Mercedes</h4>
               </div>
               <div>
                 <h4 style={{color:"#000"}}>$89 / day</h4>
@@ -75,12 +75,12 @@ function Cars() {
         </div>
         <div className='carModels'>
           <div>
-            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/passat-box.c78021e11cf230128bcc.png" alt="" />
           </div>
           <div style={{padding:"1rem"}}>
             <div className='car-second'>
               <div>
-                <h4 style={{color:"#000"}}>Audi A1</h4>
+                <h4 style={{color:"#000"}}>VW Passat</h4>
               </div>
               <div>
                 <h4 style={{color:"#000"}}>$89 / day</h4>
@@ -98,12 +98,12 @@ function Cars() {
         </div>
         <div className='carModels'>
           <div>
-            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/toyota-box.8968332b7901c6bb183c.png" alt="" />
           </div>
           <div style={{padding:"1rem"}}>
             <div className='car-second'>
               <div>
-                <h4 style={{color:"#000"}}>Audi A1</h4>
+                <h4 style={{color:"#000"}}>Toyota Camry</h4>
               </div>
               <div>
                 <h4 style={{color:"#000"}}>$89 / day</h4>
@@ -121,12 +121,12 @@ function Cars() {
         </div>
         <div className='carModels'>
           <div>
-            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/audi-box.b3263089022dd5e025dc.png" alt="" />
+            <img className='img-fluid' src="https://car-rental-ten.vercel.app/static/media/golf6-box.1982e958d19e04048c14.png" alt="" />
           </div>
           <div style={{padding:"1rem"}}>
             <div className='car-second'>
               <div>
-                <h4 style={{color:"#000"}}>Audi A1</h4>
+                <h4 style={{color:"#000"}}>Golf 6</h4>
               </div>
               <div>
                 <h4 style={{color:"#000"}}>$89 / day</h4>
