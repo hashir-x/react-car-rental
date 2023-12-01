a94fbbe4e7b497d159d2b20bb3a38b954736601d
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-12-01 18:24:19 +0530

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
   return (
     <>
       <AboutSection page={'Contact'}/>
-      <section style={{marginTop:'7rem'}}>
+      <section style={{marginTop:'7rem',marginBottom:"9rem"}}>
         <div style={{margin:'0 auto',padding:'0 2.5rem',maxWidth:"1100px"}}>
           <div className='contacts-page'>
             <div className='contact-left'>
@@ -19,20 +19,20 @@ function Contact() {
               <p className='contact'><i class="fa-solid fa-location-dot me-2"></i>Belgrade, Serbia</p>
             </div>
             <div className='contact-right'>
-              <div>
-                <label htmlFor="name">Full Name</label>
-                <input type="text" id='name' placeholder='Joe Shome'/>
+              <div className='d-flex flex-column mb-2'>
+                <label className='fw-bold mb-2' style={{color:"#000"}} htmlFor="name">Full Name</label>
+                <input className='text-contact' type="text" id='name' style={{height:"3rem",borderRadius:"4px",border:"none",backgroundColor:"#f2f2f2"}}    placeholder='Joe Shome'/>
               </div>
-              <div>
-                <label htmlFor="email">Full Name</label>
-                <input type="email" id='email' placeholder='Joe Shome@gmail.com'/>
+              <div className='d-flex flex-column mb-2'>
+                <label className='fw-bold mb-2' htmlFor="email">Full Name</label>
+                <input style={{height:"3rem",borderRadius:"4px",border:"none",backgroundColor:"#f2f2f2"}}  className='text-contact' type="email" id='email' placeholder='Joe Shome@gmail.com'/>
               </div>
-              <div>
-                <label htmlFor="about">Tell us about it</label>
-                <textarea name="about" id="about" placeholder='Write here...' cols="30" rows="10"></textarea>
+              <div className='d-flex flex-column mb-2'>
+                <label className='fw-bold mb-2' htmlFor="about">Tell us about it</label>
+                <textarea style={{borderRadius:"4px",border:"none",backgroundColor:"#f2f2f2"}} className='text-contact-area' name="about" id="about" placeholder='Write here...' cols="30" rows="10"></textarea>
               </div>
-              <div>
-                <button>Send Message</button>
+              <div className='mt-4' style={{display:"flex",width:"100%",alignItems:"center",justifyContent:"center"}}>
+                <button style={{backgroundColor:"#ff4d30",color:"#fff",padding:"1rem 2rem",border:"None",borderRadius:"4px",fontWeight:"bold"}}><i class="fa-regular fa-envelope me-2"></i>Send Message</button>
               </div>
             </div>
           </div>
