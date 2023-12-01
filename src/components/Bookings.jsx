5c73bd74412e5fcb67bf46794d2073db1c0f0478
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-12-02 01:40:59 +0530

    home

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -3,7 +3,50 @@ import React from 'react'
 function Bookings() {
   return (
     <>
-        
+        <div style={{marginTop:"30rem"}}>
+          <form style={{display:"grid",gridTemplateColumns:"repeat(3, 1fr)",gridTemplateRows:"auto",zIndex:"4",position:'absolute',justifyContent:"space-between"}}>
+            <div>
+              <label htmlFor="type">Select Your Car Type</label>
+              <select name="type" id="type">
+                <option value="audi">Audi A1 S-Line</option>
+                <option value="audis">Audi A1 S-Line</option>
+                <option value="audiss">Audi A1 S-Line</option>
+
+              </select>
+            </div>
+            <div>
+              <label htmlFor="type">Select Your Car Type</label>
+              <select name="type" id="type">
+                <option value="audi">Audi A1 S-Line</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="type">Select Your Car Type</label>
+              <select name="type" id="type">
+                <option value="audi">Audi A1 S-Line</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="type">Select Your Car Type</label>
+              <select name="type" id="type">
+                <option value="audi">Audi A1 S-Line</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="type">Select Your Car Type</label>
+              <select name="type" id="type">
+                <option value="audi">Audi A1 S-Line</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="type">Select Your Car Type</label>
+              <select name="type" id="type">
+                <option value="audi">Audi A1 S-Line</option>
+              </select>
+            </div>
+            <div></div>
+          </form>
+        </div>
     </>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import './intro.css'
+import Bookings from './Bookings'
 
 function Intro() {
   return (
     <>
         <section>
           <div style={{position:"relative",paddingTop:"5rem"}}>
-            <img style={{position:"absolute",top:"0",right:"0",zIndex:"1"}} src="https://car-rental-ten.vercel.app/static/media/hero-bg.3b5f7a2502f0f81d1490.png" alt="" />
-            <div style={{margin:'0 auto',padding:'0 2.5rem',maxWidth:"1200px",display:"flex"}}>
-              <div style={{position:"absolute",zIndex:"3",maxWidth:"500px"}}>
+            <img className='back-img' style={{position:"absolute",top:"0",right:"0",zIndex:"1"}} src="https://car-rental-ten.vercel.app/static/media/hero-bg.3b5f7a2502f0f81d1490.png" alt="" />
+            <div className='car-home' style={{margin:'0 auto',padding:'0 2.5rem',maxWidth:"1350px"}}>
+              <div style={{position:"absolute",zIndex:"3",maxWidth:"550px"}}>
                 <p style={{fontWeight:"bold",fontSize:"1.4rem",color:"#000"}}>Plan you trip now</p>
                 <h1 style={{color:"#000",fontSize:"3.6rem",fontWeight:"bolder"}}>Save <span style={{color:"#ff4d30"}}>big</span> with our car rental</h1>
-                <p style={{color:"#706f7b",marginTop:"2rem"}}>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</p>
+                <p style={{color:"#706f7b",marginTop:"2rem",textAlign:"start"}}>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</p>
                 <div className='d-flex gap-4'>
                   <button style={{border:"none",backgroundColor:"#ff4d30",color:"#fff",fontWeight:"bold",padding:"1rem 2rem",borderRadius:"4px"}}>Book Ride<i class="fa-regular fa-circle-check ms-2"></i></button>
                   <button style={{border:"none",backgroundColor:"#000",color:"#fff",fontWeight:"bold",padding:"1rem 2rem",borderRadius:"4px"}}>Learn More<i class="fa-solid fa-angle-right ms-2"></i></button>
                 </div>
               </div>
-              <img style={{position:"absolute",top:"5rem",right:"3%",zIndex:"2",width:"65%",minWidth:'400px'}} src="https://car-rental-ten.vercel.app/static/media/main-car.9b30faa59387879fa060.png" alt="" />
+              <img className='car-img' style={{position:"absolute",top:"5rem",right:"3%",zIndex:"2",width:"55%",minWidth:'540px'}} src="https://car-rental-ten.vercel.app/static/media/main-car.9b30faa59387879fa060.png" alt="" />
+              {/* <Bookings/> */}
             </div>
           </div>
         </section>
