ef2da249251f271781cfa04c173c0f3e73adae2f
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-12-02 16:34:35 +0530

    home

---
+++ src/components/CarInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CarInfo() {
+  return (
+    <>
+        <section></section>
+    </>
+  )
+}
+
+export default CarInfo
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Intro from '../components/Intro'
+import AboutSupport from '../components/AboutSupport'
+import CarInfo from '../components/CarInfo'
 
 function Home() {
   return (
     <>
     <Intro/>
+    <div style={{marginTop:"39rem",marginBottom:"5rem"}}>
+    <AboutSupport/>
+    </div>
+    <CarInfo/>
     </>
   )
 }
