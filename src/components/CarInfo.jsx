09d466a19830e72b511bd3a11dab80695460f379
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-12-02 17:11:21 +0530

    car data

--- src/components/CarInfo.jsx
+++ src/components/CarInfo.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function CarInfo() {
   return (
     <>
-        <section></section>
+        <section>
+            <div style={{margin:'0 auto',padding:'0 2.5rem',maxWidth:"1350px"}}>
+                <div className='car-info-heading'>
+                    <p>Vehicle Models</p>
+                    <h1>Our rental fleet</h1>
+                    <p>Choose from a variety of our amazing vehicles to rent for your next adventure or business trip.</p>
+                </div>
+                <div className='car-details'>
+                    <div>
+                        <button></button>
+                    </div>
+                </div>
+            </div>
+        </section>
     </>
   )
 }
---
+++ src/data/cars.js
@@ -0,0 +1,72 @@
+export const carData = [
+    {
+        'Audi A1 S-Line':{
+            carImage:'https://car-rental-ten.vercel.app/static/media/audia1.d038cf70b700e34e607a.jpg',
+            rent:"45",
+            model:'Audi',
+            mark:'A1',
+            year:'2012',
+            doors:'4/5',
+            ac:"Yes",
+            transmission:'Manual',
+            fuel:'Gasoline'
+        },
+        'VW Golf 6':{
+            carImage:'https://car-rental-ten.vercel.app/static/media/golf6.595c0bbfc7ce2e50aa05.jpg',
+            rent:"37",
+            model:'Golf 6',
+            mark:'Volkswagen',
+            year:'2008',
+            doors:'4/5',
+            ac:"Yes",
+            transmission:'Manual',
+            fuel:'Diesel'
+            },
+        'Toyota Camry':{
+            carImage:'https://car-rental-ten.vercel.app/static/media/toyotacamry.3d493a56558c50b677cc.jpg',
+            rent:"30",
+            model:'Camry',
+            mark:'Toyota',
+            year:'2006',
+            doors:'4/5',
+            ac:"Yes",
+            transmission:'Automatic',
+            fuel:'Hybrid'
+                },
+        'BMW 320 ModernLine':{
+            carImage:'https://car-rental-ten.vercel.app/static/media/bmw320.84ab73b0c9133418f907.jpg',
+            rent:"40",
+            model:'320',
+            mark:'BMW',
+            year:'2012',
+            doors:'4/5',
+            ac:"Yes",
+            transmission:'Manual',
+            fuel:'Diesel'
+            },
+        'Mercedes-Benz GLK':{
+            carImage:'https://car-rental-ten.vercel.app/static/media/benz.0a6153731a2c1a68054a.jpg',
+            rent:"50",
+            model:'Benz GLK',
+            mark:'Mercedes',
+            year:'2006',
+            doors:'4/5',
+            ac:"Yes",
+            transmission:'Manual',
+            fuel:'Gasoline'
+             },
+        'VW Passat CC':{
+            carImage:'https://car-rental-ten.vercel.app/static/media/passatcc.bd82bd1deac8c11e3c4d.jpg',
+            rent:"25",
+            model:'Passat CC',
+            mark:'Volkswagen',
+            year:'2008',
+            doors:'4/5',
+            ac:"Yes",
+            transmission:'Automatic',
+            fuel:'Gasoline'
+            },      
+    }
+
+]
+
