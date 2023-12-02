a21830d66a2d870a077786d5a0b409820e277bac
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-12-02 14:53:13 +0530

    home

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,51 +1,57 @@
 import React from 'react'
+import './booking.css'
 
 function Bookings() {
   return (
     <>
-        <div style={{marginTop:"30rem"}}>
-          <form style={{display:"grid",gridTemplateColumns:"repeat(3, 1fr)",gridTemplateRows:"auto",zIndex:"4",position:'absolute',justifyContent:"space-between"}}>
-            <div>
-              <label htmlFor="type">Select Your Car Type</label>
-              <select name="type" id="type">
-                <option value="audi">Audi A1 S-Line</option>
-                <option value="audis">Audi A1 S-Line</option>
-                <option value="audiss">Audi A1 S-Line</option>
-
-              </select>
+        <div style={{position:'relative',top:"28rem",zIndex:"4"}}>
+          <div style={{margin:'0 auto',padding:'0 2.5rem',maxWidth:"1350px"}}>
+            <div className='form-box shadow'>
+              <h3 style={{color:"#000",fontWeight:"bold"}} className='mb-4'>Book a car</h3>
+              <form className='form-data'>
+                <div>
+                  <label htmlFor="type">Select Your Car Type</label>
+                  <select name="type" id="type">
+                    <option value="audi">Audi A1 S-Lineeeeeee</option>
+                    <option value="audis">Audi A1 S-Line</option>
+                    <option value="audiss">Audi A1 S-Line</option>
+    
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="type">Select Your Car Type</label>
+                  <select name="type" id="type">
+                    <option value="audi">Audi A1 S-Line</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="type">Select Your Car Type</label>
+                  <select name="type" id="type">
+                    <option value="audi">Audi A1 S-Line</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="type">Select Your Car Type</label>
+                  <select name="type" id="type">
+                    <option value="audi">Audi A1 S-Line</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="type">Select Your Car Type</label>
+                  <select name="type" id="type">
+                    <option value="audi">Audi A1 S-Line</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="type">Select Your Car Type</label>
+                  <select name="type" id="type">
+                    <option value="audi">Audi A1 S-Line</option>
+                  </select>
+                </div>
+                <div></div>
+              </form>
             </div>
-            <div>
-              <label htmlFor="type">Select Your Car Type</label>
-              <select name="type" id="type">
-                <option value="audi">Audi A1 S-Line</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="type">Select Your Car Type</label>
-              <select name="type" id="type">
-                <option value="audi">Audi A1 S-Line</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="type">Select Your Car Type</label>
-              <select name="type" id="type">
-                <option value="audi">Audi A1 S-Line</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="type">Select Your Car Type</label>
-              <select name="type" id="type">
-                <option value="audi">Audi A1 S-Line</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="type">Select Your Car Type</label>
-              <select name="type" id="type">
-                <option value="audi">Audi A1 S-Line</option>
-              </select>
-            </div>
-            <div></div>
-          </form>
+          </div>
         </div>
     </>
   )
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,21 +6,21 @@ function Intro() {
   return (
     <>
         <section>
-          <div style={{position:"relative",paddingTop:"5rem"}}>
+          <div style={{position:"relative",paddingTop:"9rem"}}>
             <img className='back-img' style={{position:"absolute",top:"0",right:"0",zIndex:"1"}} src="https://car-rental-ten.vercel.app/static/media/hero-bg.3b5f7a2502f0f81d1490.png" alt="" />
             <div className='car-home' style={{margin:'0 auto',padding:'0 2.5rem',maxWidth:"1350px"}}>
               <div style={{position:"absolute",zIndex:"3",maxWidth:"550px"}}>
                 <p style={{fontWeight:"bold",fontSize:"1.4rem",color:"#000"}}>Plan you trip now</p>
-                <h1 style={{color:"#000",fontSize:"3.6rem",fontWeight:"bolder"}}>Save <span style={{color:"#ff4d30"}}>big</span> with our car rental</h1>
-                <p style={{color:"#706f7b",marginTop:"2rem",textAlign:"start"}}>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</p>
-                <div className='d-flex gap-4'>
+                <h1 style={{color:"#000",fontSize:"3.2rem",fontWeight:"bolder"}}>Save <span style={{color:"#ff4d30"}}>big</span> with our car rental</h1>
+                <p className='home-text' >Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</p>
+                <div className='home-button'>
                   <button style={{border:"none",backgroundColor:"#ff4d30",color:"#fff",fontWeight:"bold",padding:"1rem 2rem",borderRadius:"4px"}}>Book Ride<i class="fa-regular fa-circle-check ms-2"></i></button>
                   <button style={{border:"none",backgroundColor:"#000",color:"#fff",fontWeight:"bold",padding:"1rem 2rem",borderRadius:"4px"}}>Learn More<i class="fa-solid fa-angle-right ms-2"></i></button>
                 </div>
               </div>
-              <img className='car-img' style={{position:"absolute",top:"5rem",right:"3%",zIndex:"2",width:"55%",minWidth:'540px'}} src="https://car-rental-ten.vercel.app/static/media/main-car.9b30faa59387879fa060.png" alt="" />
-              {/* <Bookings/> */}
+              <img className='car-img' style={{position:"absolute",top:"5rem",right:"3%",zIndex:"2",width:"52%",minWidth:'550px'}} src="https://car-rental-ten.vercel.app/static/media/main-car.9b30faa59387879fa060.png" alt="" />
             </div>
+            <Bookings/>
           </div>
         </section>
     </>
