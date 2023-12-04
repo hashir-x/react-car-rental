3c804fc4d1f020323821934f360e5da03b3288bb
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-12-04 13:23:10 +0530

    home

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,51 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './booking.css'
 import { carData } from '../data/cars'
 import { placeData } from '../data/places'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Bookings() {
+
+ const [booking,setBooking] = useState({
+  car:"",pickUpLocation:"",dropOfLocation:"",pickUpTime:"",dropOfTime:""
+ })
+
+ const carBooking = (e) => {
+  e.preventDefault()
+  const {car,pickUpLocation,dropOfLocation,pickUpTime,dropOfTime} = booking
+  console.log(booking);
+  if (!car || !pickUpLocation || !dropOfLocation || !pickUpTime || !dropOfTime){
+    alert("Please fill in all data")
+  }
+ }
+
   return (
     <>
         <div style={{position:'relative',top:"28rem",zIndex:"4"}}>
           <div style={{margin:'0 auto',padding:'0 2.5rem',maxWidth:"1350px"}}>
             <div className='form-box shadow'>
               <h3 style={{color:"#000",fontWeight:"bold"}} className='mb-4'>Book a car</h3>
-              <form className='form-data'>
+              <form onSubmit={carBooking} className='form-data'>
+                {/*  */}
                 <div>
-                  <label className='required-field' htmlFor="type">Select Your Car Type</label>
-                  <select name="type" id="type" required>
+                  <label className='required-field1' htmlFor="type">Select Your Car Type</label>
+                  <select value={booking.car} onChange={e=>setBooking({...booking,car:e.target.value})} name="type" id="type" required>
                     {carData?.map((car)=>(
-                      <option value="audi">{car.name}</option>
+                      <option value={car.name}>{car.name}</option>
                       ))
                       }
                   </select>
                 </div>
+                {/*  */}
                 <div>
-                  <label className='required-field' htmlFor="pick">Pick-up</label>
-                  <select name="pick" id="pick" required>
+                  <label className='required-field2' htmlFor="pick">Pick-up</label>
+                  <select value={booking.pickUpLocation} onChange={e=>setBooking({...booking,pickUpLocation:e.target.value})} name="pick" id="pick" required>
                     {placeData?.map((place)=>(
-                      <option value="audi">{place.pickup}</option>
+                      <option value={place.pickup}>{place.pickup}</option>
                       ))
                     }
                   </select>
                 </div>
+                {/*  */}
                 <div>
-                  <label className='required-field' htmlFor="drop">Drop-of</label>
-                  <select name="drop" id="drop" required>
+                  <label className='required-field3' htmlFor="drop">Drop-of</label>
+                  <select value={booking.dropOfLocation} onChange={e=>setBooking({...booking,dropOfLocation:e.target.value})} name="drop" id="drop" required>
                     {placeData?.map((place)=>(
-                      <option value="audi">{place.drop}</option>
+                      <option value={place.drop}>{place.drop}</option>
                       ))
                       }
                   </select>
                 </div>
+                {/*  */}
                 <div>
-                  <label className='required-field' htmlFor="pickup">Pick-up</label>
-                  <input type="date" id='pickup' name='pickup'/>
+                  <label className='required-field4' htmlFor="pickup">Pick-up</label>
+                  <input value={booking.pickUpTime} onChange={e=>setBooking({...booking,pickUpTime:e.target.value})} type="date" id='pickup' name='pickup'/>
                 </div>
+                {/*  */}
                 <div>
-                  <label className='required-field' htmlFor="dropof">Drop-of</label>
-                  <input type="date" name="dropof" id="dropof" />
+                  <label className='required-field5' htmlFor="dropof">Drop-of</label>
+                  <input value={booking.dropOfTime} onChange={e=>setBooking({...booking,dropOfTime:e.target.value})} type="date" name="dropof" id="dropof" />
                 </div>
+                {/*  */}
                 <div className='d-flex justify-content-end'>
                   <button style={{backgroundColor:"#FF4D30",color:'#fff',fontWeight:'bold',padding:"0.7rem",border:"none",borderRadius:"4px"}}>Search</button>
                 </div>
